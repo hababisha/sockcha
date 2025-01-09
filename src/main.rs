fa0fc2ef1bd3827540eeb93cbb97bf640b841825
hababisha <haileamlakderbie1@gmail.com>
2025-01-10 01:05:51 +0300

    fixed bugs

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,18 @@ use tokio::sync::broadcast;
 
 mod ws_handler;
 
+
 #[tokio::main]
-async fn main(){
+async fn main() {
     let tx = Arc::new(Mutex::new(broadcast::channel(100).0));
     let tx_ws = tx.clone();
-    let ws_route = new::path("ws")
+    let ws_route = warp::path("ws")
         .and(warp::ws())
-        .map(move |ws: warp::ws::Ws |{
+        .map(move |ws: warp::ws::Ws| {
             let tx = tx_ws.clone();
-            ws.on_upgrade(move |websocket|ws_handler:handle_connection(websocket, tx))
+            ws.on_upgrade(move |websocket| ws_handler::handle_connection(websocket, tx))
         });
-        warp::serve(ws_route)
-            .run(([127,0,0,1], 8080))
-            .await;
+    warp::serve(ws_route)
+        .run(([127, 0, 0, 1], 8080))
+        .await;
 }
