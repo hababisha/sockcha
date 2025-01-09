5dcbdf2477274b9f13073dca640db4edf9d1cdb6
hababisha <haileamlakderbie1@gmail.com>
2025-01-10 00:58:47 +0300

    main fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+use std::sync::{Arc, Mutex};
+use tokio::sync::broadcast;
+
+mod ws_handler;
+
+#[tokio::main]
+async fn main(){
+    let tx = Arc::new(Mutex::new(broadcast::channel(100).0));
+    let tx_ws = tx.clone();
+    let ws_route = new::path("ws")
+        .and(warp::ws())
+        .map(move |ws: warp::ws::Ws |{
+            let tx = tx_ws.clone();
+            ws.on_upgrade(move |websocket|ws_handler:handle_connection(websocket, tx))
+        });
+        warp::serve(ws_route)
+            .run(([127,0,0,1], 8080))
+            .await;
 }
